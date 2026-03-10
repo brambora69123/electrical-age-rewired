43934d71f7d3027393a6dc53f681f0e4e72f9c6c
brambora69123 <thenicolasdev@gmail.com>
2026-03-10 19:38:57 +0100

    fix(build): implement git-based versioning and cleanup .gitignore

---
+++ src/main/resources/Version.java
@@ -0,0 +1,52 @@
+package mods.eln.misc;
+
+import static mods.eln.i18n.I18N.tr;
+
+/**
+ * Current mod version. Used to check if a new mod version is available. Must be
+ * set correctly for each mod release.
+ *
+ * @author metc
+ */
+public final class Version {
+
+    /**
+     * Major version code.
+     */
+    public final static int MAJOR = ${MAJORVERSION};
+
+    /**
+     * Minor version code.
+     */
+    public final static int MINOR = ${MINORVERSION};
+
+    /**
+     * Revision version code.
+     */
+    public final static int REVISION = ${REVISION};
+
+    /**
+     * Unique version code. Must be a String for annotations. Used to check if a
+     * new version if available. Each update must increment this number.
+     */
+    public final static int UNIQUE_VERSION = 1000000 * MAJOR + 1000 * MINOR + REVISION;
+
+    public final static String VERSION_STRING = "${VERSION_STRING}";
+
+    public static String getVersionName() {
+        return VERSION_STRING;
+    }
+
+    public static String print() {
+        return tr("mod.name") + " " + getVersionName();
+    }
+
+    public final static String printColor() {
+        return Color.WHITE + tr("mod.name") + " version "
+            + Color.ORANGE + getVersionName();
+    }
+
+    public static void main(String... args) {
+        System.out.print(getVersionName());
+    }
+}
